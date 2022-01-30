4380ab35a9083334de9d0334f747342e5c139a02
Mila <lydmiladerevjanko@gmail.com>
2022-01-30 19:14:43 +0200

    Написано решение задачи по переводу секунд в различное время.

---
+++ src/main/java/UnitChangeTask.java
@@ -0,0 +1,21 @@
+public class UnitChangeTask {
+
+    public static String changeUnit(long value) {
+
+        long remainder = value;
+        long year = remainder / 31536000;
+        remainder = remainder % 31536000;
+        long day = remainder / 86400;
+        remainder = remainder % 86400;
+        long hour = remainder / 3600;
+        remainder = remainder % 3600;
+        long minute = remainder / 60;
+        remainder = remainder % 60;
+        long second = remainder;
+
+        String result = "Your value consists of " + year + " years " +
+                day + " days " + hour + " hours " + minute + " minutes " +
+                second + " seconds ";
+        return result;
+    }
+}
